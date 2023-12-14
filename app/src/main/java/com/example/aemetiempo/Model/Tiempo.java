5035cfe45e0efa69589b01d3fcba314035d04c0f
Mariano Fernández Rodero <135753375+mferrod@users.noreply.github.com>
2023-12-14 01:20:36 +0100

    0.4.1

--- app/src/main/java/com/example/aemetiempo/Model/Tiempo.java
+++ app/src/main/java/com/example/aemetiempo/Model/Tiempo.java
@@ -1,4 +1,31 @@
 package com.example.aemetiempo.Model;
 
 public class Tiempo {
+    private String diaSemana;
+    private String maxima;
+    private String minima;
+
+    public String getDiaSemana() {
+        return diaSemana;
+    }
+
+    public String getMaxima() {
+        return maxima;
+    }
+
+    public String getMinima() {
+        return minima;
+    }
+
+    public void setDiaSemana(String diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public void setMaxima(String maxima) {
+        this.maxima = maxima;
+    }
+
+    public void setMinima(String minima) {
+        this.minima = minima;
+    }
 }
